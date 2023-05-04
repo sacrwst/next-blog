33537d95cd06e9875109f8b9410033dc7da5c3f4
sacrwst <sacrwst@gmail.com>
2023-05-05 01:08:25 +0900

    ✨ Get artcles and display their title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,35 @@
-import type { NextPage } from "next";
+import { MicroCMSListResponse } from "microcms-js-sdk";
+import type { GetStaticProps, NextPage } from "next";
+import { client } from "src/libs/client";
 
-const Home: NextPage = () => {
-  return <div className="text-blue-500">Hello!</div>;
+type Articles = {
+  title: string;
+  body: string;
+};
+
+const Home: NextPage<MicroCMSListResponse<Articles>> = (props) => {
+  console.log(props);
+
+  return (
+    <div>
+      {props.contents.map((content) => (
+        <h2 key={content.id}>{content.title}</h2>
+      ))}
+    </div>
+  );
+};
+
+export const getStaticProps: GetStaticProps<
+  MicroCMSListResponse<Articles>
+> = async () => {
+  const data = await client.getList({
+    endpoint: "articles",
+  });
+  console.log(data);
+
+  return {
+    props: data,
+  };
 };
 
 export default Home;
