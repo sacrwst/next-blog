0e6e8a429addbd198dfb204142ad7dbe269584a7
sacrwst <sacrwst@gmail.com>
2023-05-05 11:59:20 +0900

    ✨ Add blog page

---
+++ src/pages/blog/[id].tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const BlogId = () => {
+  return (
+    <div>
+      <h1>title</h1>
+      <time>time</time>
+      <div>本文</div>
+    </div>
+  );
+};
+
+export default BlogId;
