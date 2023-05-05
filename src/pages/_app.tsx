56e9eb76dbd9cf1794d668f2523e8f2efa8b56ad
sacrwst <sacrwst@gmail.com>
2023-05-06 00:06:02 +0900

    ✨ Create temp header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import "src/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="mx-auto max-w-prose">
+      <header>
+        <h1>
+          <Link href="/">Mayonaka.me</Link>
+        </h1>
+      </header>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
