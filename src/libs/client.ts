d9e7174a918d2550201568b627c90fdfdb2536b6
sacrwst <sacrwst@gmail.com>
2023-05-08 03:29:09 +0900

    🏷️ Add type definition of environment variable

--- src/libs/client.ts
+++ src/libs/client.ts
@@ -3,5 +3,5 @@ import { createClient } from "microcms-js-sdk";
 // Initialize Client SDK.
 export const client = createClient({
   serviceDomain: "mayonakadotme", // YOUR_DOMAIN is the XXXX part of XXXX.microcms.io
-  apiKey: process.env.API_KEY!,
+  apiKey: process.env.API_KEY,
 });
